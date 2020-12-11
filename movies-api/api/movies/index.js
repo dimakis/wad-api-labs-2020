c7376561a603d4f9a2c5c93811b00c20f8d36516
Dimitri Saridakis <dimitri.saridakis@gmail.com>
2020-12-11 16:26:29 +0000

    week 9 included mongoose

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -7,19 +7,12 @@ const router = express.Router();
 
 router.get('/', (req, res,next) => {
   getMovies().then(movies => res.status(200).send(movies));
-  .catch((error) => next(error));
 });
 
 router.get('/:id', (req, res, next) => {
   const id = parseInt(req.params.id);
   getMovie(id).then(movie => res.status(200).send(movie));
-  .catch((error) => next(error));
 });
-
-// router.get('/:id/reviews', (req, res, next) => {
-//   const id = parseInt(req.params.id);
-//   getMovieReviews(id).then(reviews => res.status(200).send(reviews));
-// });
 
 router.get('/:id/reviews', (req, res, next) => {
   const id = parseInt(req.params.id);
@@ -28,5 +21,3 @@ router.get('/:id/reviews', (req, res, next) => {
   .catch((error) => next(error));
 });
 export default router;
-
-
